ec046a02418f78ce2fb6ea486cb33a2da103455f
UNICE_ETU/GOM <mathias.go@etu.unice.fr>
2018-04-15 22:57:01 +0200

    [core/log] Fixup pour améliorer la vitesse d'affichage des traces et leur ordre.

--- src/core/log/log.h
+++ src/core/log/log.h
@@ -1,18 +1,22 @@
 #ifndef LOG_H
 #define LOG_H
 
+#include <stdio.h>
+
 
 #define TRACE_DBG_BASE(argModule, argFormat,...)                    \
     fprintf(    stdout,                                             \
                 "DBG: %s \t :: %s +%d (%s) \t=> " argFormat "\n",   \
                 argModule, __FILE__, __LINE__, __FUNCTION__,        \
-                ##__VA_ARGS__ );
+                ##__VA_ARGS__ );                                    \
+    fflush( stdout )
 
 #define TRACE_ERR_BASE(argModule, argFormat,...)                    \
     fprintf(    stderr,                                             \
                 "ERR: %s \t :: %s +%d (%s) \t=> " argFormat "\n",   \
                 argModule, __FILE__, __LINE__, __FUNCTION__,        \
-                ##__VA_ARGS__ );
+                ##__VA_ARGS__ );                                    \
+    fflush( stderr )
 
 
 #endif  /*< LOG_H */
