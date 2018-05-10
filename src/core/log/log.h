08f23291dbf6dbf69d48a52ceee31045a948fc48
UNICE_ETU/GOM <mathias.go@etu.unice.fr>
2018-05-10 12:44:35 +0200

    [core/log] Amélioration mineure des traces pour éviter de trop flooder stdout quand il n'y en a pas besoin.

--- src/core/log/log.h
+++ src/core/log/log.h
@@ -3,11 +3,18 @@
 
 #include <stdio.h>
 
+#ifdef  DEBUG
+#   define TRACE_PREFIX        "%s +%d (%s) \t=> "
+#   define TRACE_PREFIX_DATA   __FILE__, __LINE__, __FUNCTION__
+#else
+#   define TRACE_PREFIX        "(%s +%d) \t=> "
+#   define TRACE_PREFIX_DATA   __FUNCTION__, __LINE__
+#endif
 
 #define TRACE_DBG_BASE(argModule, argFormat,...)                    \
     fprintf(    stdout,                                             \
-                "DBG: %s \t :: %s +%d (%s) \t=> " argFormat "\n",   \
-                argModule, __FILE__, __LINE__, __FUNCTION__,        \
+                "DBG: %s \t :: " TRACE_PREFIX argFormat "\n",                \
+                argModule, TRACE_PREFIX_DATA,                                  \
                 ##__VA_ARGS__ );                                    \
     fflush( stdout )
 
